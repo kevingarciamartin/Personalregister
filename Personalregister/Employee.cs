316f41fcbafdaee0c14e2a77333268b79ff2f95f
Kevin Garcia Martin <kevingm@live.se>
2024-05-20 16:08:48 +0200

    Add Employee class

---
+++ Personalregister/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personalregister
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public double wage;
+
+        public Employee(string first, string last, double wageValue) 
+        {
+            firstName = first;
+            lastName = last;
+            wage = wageValue;
+        }
+    }
+}
